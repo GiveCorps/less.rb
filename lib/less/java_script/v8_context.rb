f1f2cbb59476ce1ff2a82147259c7f5283a142cd
Roger Tong <Roger.Tong@activenetwork.com>
2017-05-26 17:43:35 +0800

    remove lock

--- lib/less/java_script/v8_context.rb
+++ lib/less/java_script/v8_context.rb
@@ -9,10 +9,8 @@ module Less
       end
 
       def initialize(globals = nil)
-        lock do
-          @v8_context = MiniRacer::Context.new
-          globals.each { |key, val| @v8_context[key] = val } if globals
-        end
+        @v8_context = MiniRacer::Context.new
+        globals.each { |key, val| @v8_context[key] = val } if globals
       end
 
       def unwrap
@@ -20,23 +18,17 @@ module Less
       end
 
       def exec(&block)
-        lock(&block)
+        yield
       end
 
       def eval(source, options = nil) # passing options not supported
         source = source.encode('UTF-8') if source.respond_to?(:encode)
-
-        lock do
-          @v8_context.eval("(#{source})")
-        end
+        @v8_context.eval("(#{source})")
       end
 
       def call(properties, *args)
         args.last.is_a?(::Hash) ? args.pop : nil # extract_options!
-
-        lock do
-          @v8_context.eval(properties).call(*args)
-        end
+        @v8_context.eval(properties).call(*args)
       end
 
       def method_missing(symbol, *args)
@@ -46,52 +38,6 @@ module Less
           super
         end
       end
-
-      private
-
-        def lock(&block)
-          do_lock(&block)
-        rescue MiniRacer::JSError => e
-          if e.in_javascript?
-            js_value = e.value.respond_to?(:'[]')
-            name = js_value && e.value["name"]
-            constructor = js_value && e.value['constructor']
-            if name == "SyntaxError" ||
-                ( constructor && constructor.name == "LessError" )
-              raise Less::ParseError.new(e, js_value ? e.value : nil)
-            end
-          # NOTE: less/parser.js :
-          #
-          #   error = new(LessError)({
-          #      index: i,
-          #      type: 'Parse',
-          #      message: "missing closing `}`",
-          #      filename: env.filename
-          #   }, env);
-          #
-          # comes back as value: RuntimeError !
-          elsif e.value.to_s =~ /missing opening `\(`/
-            raise Less::ParseError.new(e.value.to_s)
-          end
-          raise Less::Error.new(e)
-        end
-
-        def do_lock
-          result, exception = nil, nil
-          MiniRacer::C::Locker() do
-            begin
-              result = yield
-            rescue Exception => e
-              exception = e
-            end
-          end
-
-          if exception
-            raise exception
-          else
-            result
-          end
-        end
 
     end
   end
